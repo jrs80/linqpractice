aeeeb3eac1de4bb4fb6216b3cbfe0ea4308ca4da
JRS80 <jason.steffen@gmail.com>
2022-06-27 09:51:43 -0500

    linq practice:

---
+++ LINQPractice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var gamesList = new List<string>
+            {
+                "Super Mario Bros.",
+                "Donkey Kong",
+                "Space Invaders",
+                "Centipede",
+                "Street Fighter",
+                "Tron"
+            };
+
+            Console.WriteLine("Original List:");
+            foreach(var v in gamesList) Console.WriteLine("\t" + v);
+            gamesList = gamesList.OrderBy(s=>s.Length).ToList();
+            Console.WriteLine("\nSorted List:");
+            foreach(var v in gamesList) Console.WriteLine("\t" + v);
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
